54ab725c21e2d0cce6ea115c4b6869c9bcec4f43
unknown <matheus.lima419@etec.sp.gov.br>
2023-04-21 18:34:53 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php session_start() ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Tela de Login</title>
+</head>
+<body>
+
+    <?php
+
+        if(!isset($_SESSION['login'])){
+
+            if(isset($_POST['acao'])){
+                $login = 'matheus';
+                $senha = '123456';
+
+                $loginForm = $_POST['login'];
+                $senhaForm = $_POST['senha'];
+
+                if($login == $loginForm && $senha == $senhaForm ){
+                    //logado com sucesso
+                    $_SESSION['login'] = $login;
+                    header('Location: index.php');
+                }else{
+                    //algum erro ocorreu
+                    echo 'Dados Inválidos';
+                }
+            }
+            include('login.php');
+        }else{
+            if(isset($_GET['logout'])){
+                unset($_SESSION['login']);
+                session_destroy();
+                header('Location: index.php');
+            }
+            include('home.php');
+        }
+    ?>
+</body>
+</html>
